20cbbbca4ae2e647f963922c50540a6f0b48a78e
negusu <negusuworku@gmail.com>
2015-03-17 12:16:41 +0300

    my first commit

---
+++ MvcMovie/new.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie
+{
+    public class _new : Controller
+    {
+        //
+        // GET: /new/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
